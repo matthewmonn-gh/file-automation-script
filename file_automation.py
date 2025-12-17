33b548e7a6ec2c64e860b94c774d0e9bebc1f24f
matthewmonn-gh <matthewm002@gmail.com>
2025-12-17 11:04:51 -0800

    Added more exceptions

--- file_automation.py
+++ file_automation.py
@@ -1,13 +1,20 @@
 """
-Base version of the program. 
+2nd version of the program. Flushes out exception use cases.
 """
 
-# Shell Utility. Provides a way for file and directory operations, such as copying and moving.
+# Provides a way for file and directory operations, such as copying and moving.
 import shutil
 
 # Hardcoded file to be moved and destination location
 source_file = "C:/Users/13605/Downloads/MatthewMonnResume.pdf"
-destination_location = "C:/Users/13605/Desktop/DL_Docs"
+destination_location = "C:/Users/13605/Desktop/DL_Docs/"
+
+
+# Potential issues that can occur:
+# 1. File is missing from 'source_file'
+# 2. Destination does not exist. Should it be created or just thrown an exception? This version will throw an error.
+#       Without an error source file is 'destroyed' by becoming a simple file.
+# 3. Permission issues with accessing file or destination
 
 
 def move_file(file, location):
@@ -20,8 +27,12 @@ def move_file(file, location):
     try:
         shutil.move(source_file, destination_location)
         print("File was successfully moved ")
-    except:
-        print("An error has occurred")
+    except FileNotFoundError:
+        print("Source file does not exist")
+    except PermissionError as e:
+        print("Permissions error has occured. Please adjust permission settings for file/destination and that file is closed")
+    except Exception as e:
+        print(f"Unknown error has occured: {e}")
 
 
 move_file(source_file, destination_location)
